a2992fe169aa4fcf82247087923abbf5112ad245
kamiuyzc <kamiu.yang@gmail.com>
2020-03-03 22:39:04 +0800

    add

---
+++ ddos/abnormal/get_info.py
@@ -0,0 +1,14 @@
+#!/bin/python
+import os
+import re
+
+def get_mac_and_ip(port):
+    fb = os.popen("ip addr show %s" %port)
+    data = fb.read()
+    pattern = re.compile(r'ether ((?:(?:[0-9a-f]{2}[:]){5})[0-9a-f]{2}).*inet ((?:(?:[0-9]{1,3}\.){3}[0-9]{1,3}))/', re.IGNORECASE|re.S)
+    print(data)
+    infos = pattern.search(data).groups()
+    if len(infos) == 2:
+        mac, ip = infos
+    return mac, ip 
+
--- ddos/abnormal/ip_abnormal.py
+++ ddos/abnormal/ip_abnormal.py
@@ -1,20 +1,25 @@
 import sys
 import os
 from scapy.all import *
+import get_info
+
 """
 1. stric icmp abnormal check.
 """
 
 class icmp_packet:
-    def __init__(self, src, dst):
+    def __init__(self, src, dst, smac):
         self.src = src
         self.dst = dst
+        self.smac = smac
+
     def send_icmp_less_field(self):
         """
         1. send icmp packet with 7 bytes(8 less normal).
         """
-        rawinfo = raw(IP(dst=self.dst, src=self.src)/ICMP())[0:-2]
+        rawinfo = raw(IP(dst=self.dst, src=self.src,options=[IPOption('%s%s'%('\x86\x28','a'*21))])/ICMP())[0:-2]
         pkt = IP(rawinfo)
+        pkt.psend()
         del pkt.len
         del pkt.chksum
         rep = send(pkt)
@@ -25,11 +30,26 @@ class icmp_packet:
         pkt = IP(src = self.src, dst = self.dst)/ICMP()
         pkt["IP"].chksum = 0x1111
         send(pkt)
+    def test(self):
+        pkts = rdpcap("../icmp_malformed_abnormal_bak.pcap")
+        pkt = pkts[0]
+        pkt[IP].dst = self.dst
+        pkt[IP].src = self.src 
+        #pkt[Ether].src = self.smac
+        del pkt[IP].chksum
+        del pkt[Ether].src
+        del pkt[Ether].dst
+        sendp(pkt, iface="wlp9s0")
 
 if __name__ == '__main__':
-    src = "172.22.101.138"
-    dst = sys.argv[1]
-    ob = icmp_packet(src, dst)
-    ob.send_icmp_less_field()
-    ob.send_icmp_bad_checksum()
-    ob.send_ip_bad_checksum()
+    src = "10.10.10.152"
+    if len(sys.argv) == 3:
+       port = sys.argv[2] 
+    else:
+        port = "eth1"
+
+    smac, src = get_info.get_mac_and_ip(port)
+    ob = icmp_packet(src, sys.argv[1], smac)
+    ob.test()
+    #ob.send_icmp_bad_checksum()
+    #ob.send_ip_bad_checksum()
